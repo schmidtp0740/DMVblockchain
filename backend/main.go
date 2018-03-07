9eae7ec72a0c39c2b5a0017becb40cd7dc9f0110
schmidtp0740 <schmidtp0740@gmail.com>
2018-03-07 14:17:19 -0600

    added changeOwner API

--- backend/main.go
+++ backend/main.go
@@ -21,14 +21,22 @@ type Vehicle struct {
 func newVehicle(w http.ResponseWriter, r *http.Request) {
 	var vehicle Vehicle
 	json.NewDecoder(r.Body).Decode(&vehicle)
-	fmt.Println(&vehicle)
+	fmt.Println(vehicle)
 }
 
+func changeOwner(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	var vehicle Vehicle
+	json.NewDecoder(r.Body).Decode(&vehicle)
+	fmt.Printf("vehicle vin and info: %v %v", params["vin"], vehicle)
+}
 func main() {
 	fmt.Println("hello")
 
 	router := mux.NewRouter()
 	router.HandleFunc("/newVehicle", newVehicle).Methods("POST")
+	router.HandleFunc("/changeOwner/{vin}", changeOwner).Methods("PATCH")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
