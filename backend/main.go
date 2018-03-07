b4ab9dfdc13891106425e83422c6aa6b1cf1c702
schmidtp0740 <schmidtp0740@gmail.com>
2018-03-07 14:35:35 -0600

    changed functions to global function

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -18,25 +19,42 @@ type Vehicle struct {
 	Model string `json:"model,omitempty"`
 }
 
-func newVehicle(w http.ResponseWriter, r *http.Request) {
+// NewVehicle ...
+func NewVehicle(w http.ResponseWriter, r *http.Request) {
+	// ...
 	var vehicle Vehicle
 	json.NewDecoder(r.Body).Decode(&vehicle)
 	fmt.Println(vehicle)
+
+	var jsonStr = []byte(`{
+		"channel": "mychannel",
+		"chaincode": "emrCC",
+		"chaincodeVer": "v1",
+		"args": ["", "", "", 123, "", "", "", ""]
+	}`)
+	resp, err := http.NewRequest("POST",
+		"http:129.0.0.1:4001/bcsgw/rest/v1/transaction/invocation",
+		bytes.NewBuffer(jsonStr))
+
+	fmt.Printf("Response from blockchain: %v\n%v", resp, err)
 }
 
-func changeOwner(w http.ResponseWriter, r *http.Request) {
+// ChangeOwner ...
+func ChangeOwner(w http.ResponseWriter, r *http.Request) {
+	// ...
 	params := mux.Vars(r)
 
 	var vehicle Vehicle
 	json.NewDecoder(r.Body).Decode(&vehicle)
 	fmt.Printf("vehicle vin and info: %v %v", params["vin"], vehicle)
 }
+
 func main() {
 	fmt.Println("hello")
 
 	router := mux.NewRouter()
-	router.HandleFunc("/newVehicle", newVehicle).Methods("POST")
-	router.HandleFunc("/changeOwner/{vin}", changeOwner).Methods("PATCH")
+	router.HandleFunc("/newVehicle", NewVehicle).Methods("POST")
+	router.HandleFunc("/changeOwner/{vin}", ChangeOwner).Methods("PATCH")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
