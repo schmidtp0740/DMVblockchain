14aaafe9d3bde6445f72dd1742a55ab4910f1bbc
schmidtp0740 <schmidtp0740@gmail.com>
2018-03-07 12:01:52 -0600

    added newVehicle function template

--- chaincode/chaincode.go
+++ chaincode/chaincode.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+func newVehicle(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	fmt.Println(params)
+}
 
 func main() {
 	fmt.Println("hello")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/newVehicle", newVehicle).Methods("POST")
 }
