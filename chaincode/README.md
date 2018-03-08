c8f203112f72c054ab5ee9fb004688670d244270
schmidtp0740 <schmidtp0740@gmail.com>
2018-03-08 09:36:57 -0600

    added readme file and vehicle struct to chaincode

---
+++ chaincode/README.md
@@ -0,0 +1,11 @@
+## DMV Chaincode
+
+## Prereqs
+```
+$ go get -u --tags nopkcs11 github.com/hyperledger/fabric/core/chaincode/shim
+```
+
+## Build
+```
+$ go build --tags nopkcs11
+```
--- chaincode/chaincode.go
+++ chaincode/chaincode.go
@@ -1,9 +1,35 @@
-package main
+package chaincode
 
 import (
 	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
+// Chaincode ...
+type Chaincode struct {
+	// ...
+}
+
+type vehicle struct {
+	VIN           string `json:"vin,omitempty"`
+	Year          string `json:"year,omitempty"`
+	Make          string `json:"make,omitempty"`
+	Model         string `json:"model,omitempty"`
+	Mileage       string `json:"mileage,omitempty"`
+	Salvage       string `json:"salvage,omitempty"`
+	PurchasePrice string `json:"purchasePrice,omitempty"`
+	Owner         string `json:"owner,omitempty"`
+	DOB           string `json:"dob,omitempty"`
+	StreetAddress string `json:"streetAddress,omitempty"`
+	City          string `json:"city,omitempty"`
+	State         string `json:"state,omitempty"`
+	Zip           string `json:"zip,omitempty"`
+}
+
 func main() {
-	fmt.Println("hello")
+	err := shim.Start(new(Chaincode))
+	if err != nil {
+		fmt.Printf("Error starting Chaincode: %s", err)
+	}
 }
